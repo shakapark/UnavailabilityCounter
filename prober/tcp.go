24bbc0bac243658c76268b8cc12796b6694a6fc8
Shakapark <shakapark1994@gmail.com>
2018-04-01 17:20:27 +0200

    Move conn.Close()

--- prober/tcp.go
+++ prober/tcp.go
@@ -11,13 +11,14 @@ func probeTCP(target string, timeout string) (bool, error) {
 	if r != nil {
 		return false, r
 	}
-	conn, err := net.DialTimeout("tcp4", target, d)
-	defer conn.Close()
 	
+	conn, err := net.DialTimeout("tcp4", target, d)
 	if err != nil {
 		success, err2 := probeGoogle(timeout)
 		return !success, err2
 	}
+	
+	defer conn.Close()
 
 	return true, nil
 }
